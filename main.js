75ba418d8b94bc006345bfd8f5fee18035225f9c
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-31 13:37:47 -0400

    Added input and return of letters guessed

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const express = require('express')
 const path = require('path')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 const app = express()
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const letters = []
+
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
@@ -14,7 +18,12 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', function(req, res) {
-  res.send('testing')
+  res.render('index', { letters : letters})
+})
+
+app.post('/add', function(req, res) {
+  letters.push({ letter: req.body.letterGuessed })
+  res.redirect('/')
 })
 
 app.listen(3000, function() {
