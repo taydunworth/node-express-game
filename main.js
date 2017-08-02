773f9ede468c8768109a12b79d9fe0211a90eff5
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-02 15:35:13 -0400

    Counter works properly, and winning/losing messages show

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
 const wordToGuess = words[Math.floor(Math.random() * words.length)]
 const wordLength = wordToGuess.split("")
 const guess = []
 let count = 8
+let message = ''
 
 let placeholder = wordLength.map(x => {
   return '_'
@@ -26,25 +27,50 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', function(req, res) {
-  res.render('index', { guess : guess, placeholder : placeholder, wordToGuess : wordToGuess, words : words, count: count})
+  res.render('index', { guess : guess, placeholder : placeholder, count: count, message: message })
+})
+
+app.get('/win', function(req, res) {
+  res.render('win')
+})
+
+app.get('/lose', function(req, res) {
+  res.render('lose')
 })
 
 app.post('/add', function(req, res) {
-  // Add the user's letter to the array of already guessed letters
+
+  if (guess.includes(req.body.letterGuessed)) {
+    console.log('you have already guessed that letter')
+    res.redirect('/')
+  } else if (wordLength.includes(req.body.letterGuessed)) {
+    wordLength.forEach(function(letter, index) {
+      // if that letter is the letter the user guessed
+      if (letter === req.body.letterGuessed) {
+        // Replace that *INDEX* within the placeholder with the letter
+        placeholder[index] = letter
+      }
+    })
+  } else {
+    count -= 1
+    if (placeholder.join(',') != wordLength.join(',') && count <= 0) {
+      message = 'You lose!'
+      console.log(message)
+      res.redirect('/lose')
+    }
+  }
+
   guess.push({ letter: req.body.letterGuessed })
 
-  // go through the secret word one letter at time
-  wordLength.forEach(function(letter, index) {
 
-    // if that letter is the letter the user guessed
-    if (letter === req.body.letterGuessed) {
-      // Replace that *INDEX* within the placeholder with the letter
-      placeholder[index] = letter
-    }
-  })
-
+  if (placeholder.join(',') === wordLength.join(',') && count >= 0) {
+    message = 'You win!'
+    console.log(message)
+    res.redirect('/win')
+  }
   res.redirect('/')
 })
+
 
 app.listen(3000, function() {
   console.log('Successfully started express application!')
